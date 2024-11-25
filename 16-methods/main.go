c3392bf5c751746fe9e4215ccfbf6b5d327e5ecc
sreeram780 <sreenu.ram2@gmail.com>
2024-11-25 16:46:46 +0530

    methods in golang

---
+++ 16-methods/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to struct in go")
+	// no inheritance in golang, no parent, and child or super
+	sree := User{
+		Name:   "sree",
+		Email:  "t@t.com",
+		Status: true,
+		Age:    31,
+	}
+	fmt.Println("Welcome to struct user:", sree)
+	fmt.Printf("sree details are %+v \n", sree)
+	fmt.Printf("Name is %v and mail %v \n", sree.Name, sree.Email)
+	// calling method
+	sree.GetStatus()
+	// updating prop
+	sree.NewMail()
+}
+
+// struct
+// make name and properties start with capital letter
+type User struct {
+	Name   string
+	Email  string
+	Status bool
+	Age    int
+}
+
+func (u User) GetStatus() {
+	fmt.Println("is user active: ", u.Status)
+}
+
+func (u User) NewMail() {
+	u.Email = "udatedmail@here"
+	fmt.Println("Email of this user is: ", u.Email)
+}
